712b4ed5c1da8ff2b546f7f7791d634c71fffa81
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-10-20 12:41:05 +0600

    add cart

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ async function run() {
     // get single user 
     app.get('/users/:id', async(req, res)=>{
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = {email:id};
       const result = await usersCollection.findOne(query)
       res.send(result)
   })
